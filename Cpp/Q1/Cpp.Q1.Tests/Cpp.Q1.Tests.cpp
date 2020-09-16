65d59f45496d8e7507fd6e2e7010d92dfae4b3ec
Pierre-Luc Griffin <pierre-luc.griffin@omnirobotic.com>
2020-09-16 15:17:11 -0400

    Add Cpp Q1.

---
+++ Cpp/Q1/Cpp.Q1.Tests/Cpp.Q1.Tests.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include <future>
+
+#include "Shop.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CppQ1Tests
+{
+    TEST_CLASS(CppQ1Tests)
+    {
+    public:
+
+        TEST_METHOD(synchronization)
+        {
+            int nb_command_to_add = 100000;
+
+            Shop shop;
+            auto add_task = std::async(&Shop::add_commands, &shop, nb_command_to_add);
+            auto consume_task = std::async(&Shop::consume_commands, &shop, nb_command_to_add);
+
+            add_task.wait();
+            consume_task.wait();
+
+            Assert::AreEqual(0, shop._nb_command_pending);
+
+            auto last_consumed_command = consume_task.get();
+            Assert::AreEqual(nb_command_to_add - 1, last_consumed_command);
+        }
+    };
+}
