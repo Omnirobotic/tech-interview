592772d2542774a432eb99ec03df19f768989456
Pierre-Luc Griffin <pierre-luc.griffin@omnirobotic.com>
2020-10-05 11:19:13 -0400

    Add bit more info to help understand CppQ1.

--- Cpp/Q1/Cpp.Q1.Tests/Cpp.Q1.Tests.cpp
+++ Cpp/Q1/Cpp.Q1.Tests/Cpp.Q1.Tests.cpp
@@ -14,6 +14,9 @@ namespace CppQ1Tests
     public:
 
         ///TODO: Modify Shop.h to make sure this test passes all the time and explain why it might pass sometimes.
+        //This test simulate the case where you have a thread that is generating data and another thread 
+        // that is consuming this data. We need to make sure there is no memory corruption.
+        //By the way, if the test doesn't run (ends without the green/red) it means there was a memory access violation.
         TEST_METHOD(CppQ1Test)
         {
             int nb_command_to_add = 100000;
--- Cpp/Q1/README.md
+++ Cpp/Q1/README.md
@@ -1,4 +1,7 @@
 # Cpp Question 1
 Your goal for this challenge is to modify Shop.h to make sure this test passes all the time and explain why it might pass sometimes.
 
+This test simulate the case where you have a thread that is generating data and another thread 
+that is consuming this data. We need to make sure there is no memory corruption.
+By the way, if the test doesn't run (ends without the green/red) it means there was a memory access violation.
 
