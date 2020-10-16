9ad56e7bcb0e6266e6ca11d303b926d4fd30e9fd
Pierre-Luc Griffin <pierre-luc.griffin@omnirobotic.com>
2020-10-16 07:54:22 -0400

    Add more info to guide on the expected solution.

--- Cpp/Q1/Cpp.Q1.Tests/Shop.h
+++ Cpp/Q1/Cpp.Q1.Tests/Shop.h
@@ -8,11 +8,14 @@ class Shop
 public:
     int _nb_command_pending = 0;
 
-    void add_commands(int nb_command)
+    void create_commands(int nb_command)
     {
         int i = 0;
         while (i < nb_command)
         {
+            //Imagine some processing to create a command.
+            //This processing could take 10 ms.
+
             _commands.push_back(i);
             _nb_command_pending++;
             i++;
@@ -32,6 +35,9 @@ public:
             _commands.erase(_commands.begin());
             _nb_command_pending--;
             nb_command_consumed++;
+
+            //Imagine some processing with last_command_consumed here.
+            //This processing could take 9 ms.
         }
 
         return last_command_consumed;
